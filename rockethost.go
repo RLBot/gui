46e5dc0d1e4c65a89ec5723ae1de676a37ead925
VirxEC <virx@virxcase.dev>
2025-03-15 01:17:42 -0400

    Fix RocketHost match start

--- rockethost.go
+++ rockethost.go
@@ -162,15 +162,7 @@ func (a *App) StartRHostMatch(settings RHostMatchSettings) (string, error) {
 
 	err = conn.SendPacket(&flat.MatchConfigurationT{
 		PlayerConfigurations: []*flat.PlayerConfigurationT{},
-		ScriptConfigurations: []*flat.ScriptConfigurationT{
-			{
-				Name:       "GUIv5",
-				RootDir:    "",
-				RunCommand: "",
-				SpawnId:    0,
-				AgentId:    "rlbot/gui",
-			},
-		},
+		ScriptConfigurations: []*flat.ScriptConfigurationT{},
 		GameMode:              flat.GameModeSoccer,
 		Mutators:              &flat.MutatorSettingsT{},
 		ExistingMatchBehavior: flat.ExistingMatchBehaviorRestart,
@@ -185,7 +177,7 @@ func (a *App) StartRHostMatch(settings RHostMatchSettings) (string, error) {
 	}
 
 	err = conn.SendPacket(&flat.ConnectionSettingsT{
-		AgentId:              "rlbot/gui",
+		AgentId:              "",
 		WantsBallPredictions: false,
 		WantsComms:           false,
 		CloseBetweenMatches:  false,
